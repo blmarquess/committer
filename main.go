54ee7492a9be4cfc4c19100ca0ea2d9b04ece33b
Bruno Marques <blmarques.dev@gmail.com>
2023-12-31 19:14:24 -0300

    feat(files): extract funcs of main to utils and clean app

--- main.go
+++ main.go
@@ -1,77 +1,52 @@
 package main
 
 import (
-	"bufio"
 	"commiter/modules"
 	"fmt"
 	"os"
-	"strings"
 )
 
 func main() {
+	// options := modules.ListOptions{
+	// 	Items: []modules.Item{
+	// 		modules.Item{Title: "feat", Desc: "nova funcionalidade"},
+	// 		modules.Item{Title: "fix", Desc: "descrição"},
+	// 		modules.Item{Title: "docs", Desc: "descrição"},
+	// 		modules.Item{Title: "style", Desc: "descrição"},
+	// 		modules.Item{Title: "refactor", Desc: "descrição"},
+	// 		modules.Item{Title: "test", Desc: "descrição"},
+	// 		modules.Item{Title: "chore", Desc: "descrição"},
+	// 		modules.Item{Title: "build", Desc: "descrição"},
+	// 		modules.Item{Title: "ci", Desc: "descrição"},
+	// 		modules.Item{Title: "perf", Desc: "descrição"},
+	// 		modules.Item{Title: "revert", Desc: "descrição"},
+	// 	},
+	// 	Title: "Qual o tipo de commit?",
+	// }
 	options := modules.ListOptions{
 		Items: []string{"feat", "fix", "docs", "style", "refactor", "test", "chore", "build", "ci", "perf", "revert"},
 		Title: "Qual o tipo de commit?",
 	}
+	var commit string
 
 	commitType, err := modules.CreateList(options)
 	if err != nil {
 		fmt.Printf("Error: %s\n", err)
 		os.Exit(1)
 	}
-	commit := getInput("Descrição curta: ", "")
-	res := condition("Adicionar uma descrição longa no commit?", false)
-	var longDescription string
-	if res {
-		longDescription = getInput("Descrição longa", "")
-	}
-	res = condition("Adicionar uma issue no commit?", false)
-	var issue string
-	if res {
-		issue = getInput("Issue", "")
-	}
-	// return modules.Commit(commitType, commit, longDescription, issue)
+	commit = commit + commitType
+	commit = commit + modules.GetInput("Descrição curta: ", "")
 
-	fmt.Printf("Qual o tipo de commit? %s\n", commitType)
-	commitResult := "git commit -m \"" + "(" + commitType + "):" + commit + "\n\n" + longDescription + "\n\n " + issue + "\""
-	fmt.Printf(commitResult)
-}
-
-func getInput(label string, def string) string {
-	reader := bufio.NewReader(os.Stdin)
-	var inputs string
-	for {
-		fmt.Fprintf(os.Stderr, "%s", label)
-		inputs, _ = reader.ReadString('\n')
-		inputs = strings.TrimSpace(inputs)
-		if inputs == "" {
-			return def
-		}
-		return inputs
-	}
-}
-
-func condition(label string, def bool) bool {
-	choices := "Y/n"
-	if !def {
-		choices = "y/N"
+	if modules.Condition("Adicionar uma descrição longa no commit?", false) {
+		commit = commit + "\n\n" + modules.GetInput("Descrição longa", "")
 	}
 
-	reader := bufio.NewReader(os.Stdin)
-	var inputs string
-	for {
-		fmt.Fprintf(os.Stderr, "%s (%s) ", label, choices)
-		inputs, _ = reader.ReadString('\n')
-		inputs = strings.TrimSpace(inputs)
-		if inputs == "" {
-			return def
-		}
-		inputs = strings.ToLower(inputs)
-		if inputs == "y" || inputs == "yes" || inputs == "s" || inputs == "sim" {
-			return true
-		}
-		if inputs == "n" || inputs == "no" || inputs == "não" || inputs == "nao" {
-			return false
-		}
+	if modules.Condition("Adicionar uma id de alguma issue ?", false) {
+		commit = commit + "\n\n" + "#" + modules.GetInput("Issue: ", "")
 	}
+
+	fmt.Println(commit)
+
+	// res := modules.GetListWithDesc()
+	// fmt.Println(res)
 }
