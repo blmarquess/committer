77b97ebb8dcac56308568edcb641119d30409a2b
Bruno Marques <blmarques.dev@gmail.com>
2024-01-20 23:51:49 -0300

    feat(style): adds formatter of commits with multiline style

---
+++ modules/formatter.go
@@ -0,0 +1,23 @@
+package modules
+
+func Formatter(c Commit) string {
+	var commit string
+
+	commit = commit + c.CommitType
+
+	if c.Scope != "" {
+		commit = commit + "(" + c.Scope + ")"
+	}
+
+	commit = commit + ": " + c.ShortDesc
+
+	if c.LongDesc != "" {
+		commit = commit + "\n\n" + c.LongDesc
+	}
+
+	if c.Issue != "" {
+		commit = commit + "\n\n" + "#" + c.Issue
+	}
+
+	return commit
+}
