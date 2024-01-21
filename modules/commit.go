0ac230c882678a7f2e85df5b7716ef50640aa27d
Bruno Marques <blmarques.dev@gmail.com>
2024-01-21 00:22:00 -0300

    feat: add Commit entity

---
+++ modules/commit.go
@@ -0,0 +1,9 @@
+package modules
+
+type Commit struct {
+	CommitType string
+	Scope      string
+	ShortDesc  string
+	LongDesc   string
+	Issue      string
+}
--- modules/formatter.go
+++ modules/formatter.go
@@ -2,22 +2,16 @@ package modules
 
 func Formatter(c Commit) string {
 	var commit string
-
 	commit = commit + c.CommitType
-
 	if c.Scope != "" {
 		commit = commit + "(" + c.Scope + ")"
 	}
-
 	commit = commit + ": " + c.ShortDesc
-
 	if c.LongDesc != "" {
 		commit = commit + "\n\n" + c.LongDesc
 	}
-
 	if c.Issue != "" {
 		commit = commit + "\n\n" + "#" + c.Issue
 	}
-
 	return commit
 }
